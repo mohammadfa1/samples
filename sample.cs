cd09afe47f20b9e744e17cfe52ed7ed35e2ce1cc
mohammadfa1 <108895830+mohammadfa1@users.noreply.github.com>
2022-07-07 22:59:10 +0430

    Create sample.cs

---
+++ sample.cs
@@ -0,0 +1,6 @@
+System.Console.WriteLine("enter your number: ");
+int num=int.Parse(Console.ReadLine());
+int te=0;
+for(int i=0;i<=num;i++){
+te=te+i;
+}System.Console.WriteLine("sum of "+num +" to 1: "+te);
